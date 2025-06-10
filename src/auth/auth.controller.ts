44f6a7f8b30b3cb47655c8453e2ed9bb2b7ef1b1
Pr0Solutti <contato@prosolutti.com>
2025-06-10 18:32:15 -0300

    fix: auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -34,9 +34,10 @@ export class AuthController {
       serialize('@peticaoinicial:accessToken', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // true em produção com HTTPS
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         path: '/',
         maxAge: 60 * 60 * 24 * 7, // 7 dias,
+        domain: '.up.railway.app',
       }),
     );
 
